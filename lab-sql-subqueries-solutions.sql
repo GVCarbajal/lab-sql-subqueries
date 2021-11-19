e08fddbab202e16d8eb632ede57c0ef0a12ddfc5
GVarela <gvarelacarbajal@gmail.com>
2021-11-19 12:25:48 +0100

    lab SQL subqueries solutions

--- lab-sql-subqueries-solutions.sql
+++ lab-sql-subqueries-solutions.sql
@@ -28,33 +28,43 @@ where film_id in
 select category_id from category where name = "Family"));
 
 
-/* Get name and email from customers from Canada using subqueries. Do the same with joins. 
-Note that to create a join, you will have to identify the correct tables with their primary keys 
-and foreign keys, that will help you get the relevant information. */
+-- Get name and email from customers from Canada using subqueries. Do the same with joins.
 
 -- SUBQUERRIES
-
 select first_name, last_name, email from customer where address_id in (
 select address_id from address where city_id in
 (select city_id from city where country_id = ( 
 select country_id from country where country = "Canada")));
 
 -- jOINS
+select first_name, last_name, email from customer c
+join address a on c.address_id = a.address_id
+join city m on a.city_id = m.city_id
+join country p on m.country_id = p.country_id
+where country = "Canada";
 
 
+-- Which are films starred by the most prolific actor?
 
-
-/* Which are films starred by the most prolific actor? Most prolific actor is defined as the 
-actor that has acted in the most number of films. First you will have to find the most prolific 
-actor and then use that actor_id to find the different films that he/she starred. */
-
-
+select title from film f join film_actor a on f.film_id = a.film_id
+where a.actor_id = (select a.actor_id from actor a join film_actor f on a.actor_id = f.actor_id
+group by a.actor_id order by count(f.film_id) desc limit 1);
 
 
 /* Films rented by most profitable customer. You can use the customer table and payment table 
 to find the most profitable customer ie the customer that has made the largest sum of payments */
 
-
+select title from rental r
+join inventory i on r.inventory_id = i.inventory_id
+join film f on i.film_id = f.film_id
+where customer_id = (
+select customer_id from payment group by customer_id order by sum(amount) desc limit 1);
 
 
 -- Customers who spent more than the average payments.
+
+select first_name, last_name, email from customer
+where customer_id in (select customer_id from payment group by customer_id
+having sum(amount) > (select avg(paid) from customer c
+join (select customer_id, sum(amount) paid from payment group by customer_id) a
+on c.customer_id = a.customer_id));
